6b68a81aab52b6c155c2eedb57bf3b94aa127893
Anthony Oakley <agoakley@gmail.com>
2025-03-13 19:36:29 +0000

    u45

--- modules/utilities.lua
+++ modules/utilities.lua
@@ -893,3 +893,33 @@ function L.Trim(stringValue)
 
     return trimmed
 end
+
+---update 45 has removed this function for some reason
+---@param achievementId number
+---@return number
+function L.GetAchievementStatus(achievementId)
+    local completed = 0
+    local total = 0
+    local numCriteria = GetAchievementNumCriteria(achievementId)
+
+    for criterionIndex = 1, numCriteria do
+        local _, numCompleted, numRequired = GetAchievementCriterion(achievementId, criterionIndex)
+
+        completed = completed + numCompleted
+        total = total + numRequired
+    end
+
+    if total > 0 then
+        if completed > 0 then
+            if completed == total then
+                return ZO_ACHIEVEMENTS_COMPLETION_STATUS.COMPLETE
+            else
+                return ZO_ACHIEVEMENTS_COMPLETION_STATUS.IN_PROGRESS
+            end
+        else
+            return ZO_ACHIEVEMENTS_COMPLETION_STATUS.INCOMPLETE
+        end
+    end
+
+    return ZO_ACHIEVEMENTS_COMPLETION_STATUS.NOT_APPLICABLE
+end
